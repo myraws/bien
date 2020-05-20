11d17e7ce7357d8da1e5fb203e02ec30461f8ff0
myra <myraliws@gmail.com>
2020-05-20 19:47:53 +0900

    added ambiance and SEO

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -58,7 +58,7 @@ class ReviewsController < ApplicationController
   end
 
   def form_params
-    params.require(:review).permit(:title, :body, :score)
+    params.require(:review).permit(:title, :body, :score, :restaurant, :ambiance)
   end
 
 end
